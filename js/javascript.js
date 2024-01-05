9b7319bba78b38e4dbb749cfb0a985b276e9e185
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-01-04 23:07:07 -0600

    escuchando eventos con javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,23 @@
 
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
 
 
 
 function seleccionarMascotaJugador(){
-    alert("Selecio")
+
+    if (document.getElementById('hypodoge').checked){
+        alert("Seleccionaste a Hypodoge")
+    }
 }
+
+window.addEventListener('load',iniciarJuego)
+
+
+function iniciarJuego (){
+     let botonMascotaJugador = document.getElementById('boton-mascota')
+      botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+}
+
+/*saber cual de los circulos fue seleccionado*/ 
