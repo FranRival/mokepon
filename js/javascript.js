482396cf6e73b4a1e01ab174634dd3a9925b2523
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-01-05 22:48:44 -0600

    enemigos aleatorios

--- js/javascript.js
+++ js/javascript.js
@@ -21,6 +21,7 @@ function seleccionarMascotaJugador(){
         alert ("Tienes que selecionar")
 
     }
+    seleccionarMascotaEnemigo()
     
 }
 
@@ -30,9 +31,22 @@ window.addEventListener('load',iniciarJuego)
 function iniciarJuego (){
      let botonMascotaJugador = document.getElementById('boton-mascota')
       botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
-
 }
 
 
-//innerHTML es para leer el interior de las etiquetas HTML
-//tambien se puede modificar
+function aleatorio (min, max){
+    return Math.floor(Math.random()*(max-min +1)+ min)
+}
+
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio (1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio==1){
+        spanMascotaEnemigo.innerHTML='Hipodoge'
+    }else if (ataqueAleatorio==2){
+        spanMascotaEnemigo.innerHTML='Capipepo'
+    }else if (ataqueAleatorio==3){
+        spanMascotaEnemigo.innerHTML='Ratigueya'
+    }
+}
