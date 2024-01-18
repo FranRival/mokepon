2c34c0265fbe7768f1baa5bc75fb08535a75fbde
Emmanuel Ibarra <152581188+FranRival@users.noreply.github.com>
2024-01-17 23:00:14 -0600

    Otra ubicacion a partir del error...

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,8 @@
 
 
 //el error esta desde la funcion de ataqueAgua, ataqueFuego, ataqueTierra. En adelante. 
+//o posiblemente el error este en la funcion de combate. ya que funcionaria bien la funcion de ataque aleatorioenemigo
+
 
 function seleccionarMascotaJugador(){
     let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque').style.display = 'block'
